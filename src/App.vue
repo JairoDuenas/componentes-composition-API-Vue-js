a38da8f6f6ab62252ecbf4cacc17dfa53773bfc4
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-09 06:45:30 -0500

    Transiciones

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
   <HelloWorld />
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import Menu from "./components/Menu.vue";
 
 const HelloWorld = defineAsyncComponent(() =>
   import("./components/HelloWorld.vue")
@@ -15,6 +18,12 @@ export default {
   name: "App",
   components: {
     HelloWorld,
+    Menu,
+  },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
@@ -27,5 +36,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <ul>
+    <li>Opción 1</li>
+    <li>Opción 2</li>
+    <li>Opción 3</li>
+  </ul>
+</template>
